02c8d0ea67af334373a76c14a0690f39c329c72c
maidou0215 <408296372@qq.com>
2017-05-17 15:55:59 +0800

    修正刷新

--- app/src/main/java/com/nov/openview/utils/MyRippleLayout.java
+++ app/src/main/java/com/nov/openview/utils/MyRippleLayout.java
@@ -2,7 +2,9 @@ package com.nov.openview.utils;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -26,10 +28,12 @@ public class MyRippleLayout extends FrameLayout {
 
     private Paint mCirclePaint;
     private Paint mPaint;
-    private float end_radial = ViewConvertUtil.dip2px(getContext(), 200);
-    private int color = R.color.colorWhite;
+    private static final float DEFAULT_CIRCLE_RADIAL = 200;
+    private static final int DEFAULT_DURATION = 2400;
+    private float end_radial, start_radial, img_radial;
+    private int circle_color, duration;
     private ArrayList<ValueAnimator> mAnimators;
-    float[] scaleFloats = new float[]{end_radial / 3, end_radial / 3, end_radial / 3, end_radial / 3};
+    float[] scaleFloats = new float[]{start_radial, start_radial, start_radial, start_radial};
     int[] alphaInts = new int[]{255, 255, 255, 255};
 
     RippleImageView mRippleImageView;
@@ -41,17 +45,33 @@ public class MyRippleLayout extends FrameLayout {
 
     public MyRippleLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initView(context, attrs);
+    }
+
+    private void initView(Context context, AttributeSet attrs) {
+        TypedArray attributeArray = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.my_ripple_view, 0, 0);
+        end_radial = attributeArray.getFloat(R.styleable.my_ripple_view_view_radius, DEFAULT_CIRCLE_RADIAL);
+        circle_color = attributeArray.getColor(R.styleable.my_ripple_view_view_color, Color.WHITE);
+        duration = attributeArray.getInteger(R.styleable.my_ripple_view_view_duration, DEFAULT_DURATION);
+        end_radial = ViewConvertUtil.dip2px(context, end_radial);
+        start_radial = end_radial / 3;
+        img_radial = end_radial * 2 / 3;
         mRippleImageView = new RippleImageView(context);
-        FrameLayout.LayoutParams rippleParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        FrameLayout.LayoutParams imgParams = new FrameLayout.LayoutParams((int)end_radial*2/3, (int)end_radial*2/3);
+        LayoutParams rippleParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        LayoutParams imgParams = new LayoutParams((int)img_radial, (int)img_radial);
         imgParams.gravity = Gravity.CENTER;
         rippleParams.gravity = Gravity.CENTER;
         mCircleImageView = new CircleImageView(context);
         mCircleImageView.setImageResource(R.drawable.ic_w);
         mCircleImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         mCircleImageView.setLayoutParams(imgParams);
+
         addView(mRippleImageView, rippleParams);
         addView(mCircleImageView);
+    }
+
+    public void setRippleColor(int color) {
+        mRippleImageView.setRippleColor(color);
     }
 
     public void startAnimation() {
@@ -78,15 +98,29 @@ public class MyRippleLayout extends FrameLayout {
         private void initPaint() {
             mCirclePaint = new Paint();
             mCirclePaint.setAntiAlias(true);
-            mCirclePaint.setColor(getResources().getColor(color));
+            mCirclePaint.setColor(circle_color);
             mCirclePaint.setStyle(Paint.Style.STROKE);
             mCirclePaint.setStrokeWidth(10);
-
             mPaint = new Paint();
             mPaint.setAntiAlias(true);
-            mPaint.setColor(getResources().getColor(color));
+            mPaint.setColor(circle_color);
             mPaint.setStyle(Paint.Style.STROKE);
             mPaint.setStrokeWidth(10);
+        }
+
+        public void setRippleColor(int color) {
+            circle_color = color;
+            invalidate();
+        }
+
+        public void setDuration(int d) {
+            duration = d;
+            invalidate();
+        }
+
+        public void setRadial(float radial) {
+            end_radial = radial;
+            invalidate();
         }
 
         @Override
@@ -96,15 +130,15 @@ public class MyRippleLayout extends FrameLayout {
                 mCirclePaint.setAlpha(alphaInts[i]);
                 canvas.drawCircle(getWidth() / 2, getHeight() / 2, scaleFloats[i], mCirclePaint);
             }
-            canvas.drawCircle(getWidth() / 2, getHeight() / 2, end_radial / 3, mPaint);
+            canvas.drawCircle(getWidth() / 2, getHeight() / 2, start_radial, mPaint);
         }
 
         public ArrayList<ValueAnimator> onCreateAnimators() {
             ArrayList<ValueAnimator> animators = new ArrayList<>();
-            long[] delays = new long[]{0, 800, 1600, 2400};
+            long[] delays = new long[]{0, duration / 3, duration * 2 / 3, duration};
             for (int i = 0; i < 4; i++) {
                 final int index = i;
-                ValueAnimator scaleAnim = ValueAnimator.ofFloat(end_radial / 3, end_radial);
+                ValueAnimator scaleAnim = ValueAnimator.ofFloat(start_radial, end_radial);
                 scaleAnim.setDuration(2400);
                 scaleAnim.setInterpolator(new LinearInterpolator());
                 scaleAnim.setRepeatCount(-1);
